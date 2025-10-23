e20a7eb08fbb508024e285b5070b537de512459c
PITSO  TLADI <402104617@my.richfield.ac.za>
2025-10-23 15:10:02 +0200

    oopsie on db file

--- backend/sql/bubble_data (1).sql
+++ backend/sql/bubble_data (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2025 at 02:58 PM
+-- Generation Time: Oct 23, 2025 at 03:07 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -61,8 +61,11 @@ CREATE TABLE `messages` (
   `id` int(11) NOT NULL,
   `sender_id` int(11) NOT NULL,
   `receiver_id` int(11) NOT NULL,
-  `message_text` text NOT NULL,
-  `sent_at` datetime DEFAULT current_timestamp()
+  `content` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `is_read` tinyint(1) DEFAULT 0,
+  `reply_to_id` int(11) DEFAULT NULL,
+  `message_type` varchar(20) DEFAULT 'text'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -88,7 +91,12 @@ INSERT INTO `posts` (`id`, `content`, `time_posted`, `user_id`, `image_path`, `t
 (13, 'dreams are the wings of the mind you can fly\r\n', '2025-10-20 11:05:19', 15, NULL, '2025-10-20 09:05:19'),
 (20, 'im wishing on a star', '2025-10-20 12:08:16', 14, NULL, '2025-10-20 10:08:16'),
 (22, 'what a man what a night', '2025-10-20 12:49:12', 12, '68f613a8d704d_527d94df492c87037c2f424374de9056.jpg', '2025-10-20 10:49:12'),
-(23, 'Son we live in a world with walls, walls that have to be protected by man with guns. Who is gonna do it you, you lieutenant Weinberg? I have a responsibility greater than you could possibly ever fathom. You weep for Santiago, and you curse the marines, you have that luxury, the luxury of not knowing what I know, that Santiago\'s death while tragic probably saved lives and my existence while grotesque and incomprehensible to you saves lives!!!you don\'t want the truth because deep down in places you don\'t talk about at parties, you want me on that wall you need me on that wall.we use words like honor code and respect we use these words as the backbone of a life spent protecting something,you use them as a punchline!!!', '2025-10-20 14:31:51', 12, '68f62bb736c87_Colonel Nathan R_ Jessup - \'A Few Good Men\' (1).jpg', '2025-10-20 12:31:51');
+(23, 'Son we live in a world with walls, walls that have to be protected by man with guns. Who is gonna do it you, you lieutenant Weinberg? I have a responsibility greater than you could possibly ever fathom. You weep for Santiago, and you curse the marines, you have that luxury, the luxury of not knowing what I know, that Santiago\'s death while tragic probably saved lives and my existence while grotesque and incomprehensible to you saves lives!!!you don\'t want the truth because deep down in places you don\'t talk about at parties, you want me on that wall you need me on that wall.we use words like honor code and respect we use these words as the backbone of a life spent protecting something,you use them as a punchline!!!', '2025-10-20 14:31:51', 12, '68f62bb736c87_Colonel Nathan R_ Jessup - \'A Few Good Men\' (1).jpg', '2025-10-20 12:31:51'),
+(24, 'genlemen that night is upon us', '2025-10-20 14:59:25', 14, '68f6322dde85b_lfc.floz67 - 7546249510352194818.mp4', '2025-10-20 12:59:25'),
+(25, 'nothin much', '2025-10-21 10:09:54', 14, NULL, '2025-10-21 08:09:54'),
+(26, 'the path of the righteous man is beset on all sides by the inequities of the selfish and the tyranny of evil men. Blessed is he who in the name of charity Shepards his brother through the valley of darkness for he is truly his brother\'s keeper and the finder of lost children. I will strike upon thee with great vengeance and furious anger those who would poison and destroy my brothers. And you will know my name is the lord when I lay my vengeance upon thee!!', '2025-10-22 10:53:31', 20, '68f89b8b25bbe_Samuel L Jackson.jpg', '2025-10-22 08:53:31'),
+(27, 'We’re the middle children of history, man. No purpose or place. We have no Great War. No Great Depression. Our Great War’s a spiritual war… our Great Depression is our lives. We’ve all been raised on television to believe that one day we’d all be millionaires, and movie gods, and rock stars. But we won’t. And we’re slowly learning that fact. And we’re very, very pissed off', '2025-10-22 11:47:11', 20, '68f8a81fd8b8f_download.jpg', '2025-10-22 09:47:11'),
+(28, 'when in disgrace with fortune and man\'s eyes, I all alone beweep my outcast state and bother deaf heaven with my bootless cries, wishing me like to one more rich in hope, featured like him, like him with friends possessed, desiring this man\'s art and that man\'s scope, with what I most enjoy contented least. Yet in these thoughts myself almost despising, haply I think on thee, and my state sings hymns at heaven\'s gate, for thy sweet love remembered such wealth brings that I then scorn to change my state with kings', '2025-10-22 15:07:16', 15, '68f8d704c3024_shakespare.jpg', '2025-10-22 13:07:16');
 
 -- --------------------------------------------------------
 
@@ -104,19 +112,22 @@ CREATE TABLE `users` (
   `profile_picture` varchar(255) DEFAULT NULL,
   `security_question` varchar(255) NOT NULL,
   `security_answer` varchar(255) DEFAULT NULL,
-  `remember_token` varchar(255) DEFAULT NULL
+  `cover_pic` varchar(500) DEFAULT NULL,
+  `bio` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `full_name`, `email`, `password_hash`, `profile_picture`, `security_question`, `security_answer`, `remember_token`) VALUES
-(12, 'Pitso Tladi', 'pitsotladi71@gmail.com', '$2y$10$Lia0F6iXjt5RqiMrqdRWkOWe/I4Y9jZgPJAAKK/U85uI6U4tRe33m', 'OIP.webp', 'city of birth', 'Johannesburg', NULL),
-(13, 'george orwell', 'georgeOrwell@oceana.gov', '$2y$10$gwB5LU8riewZiXHYYl7DKumXgqqe8tOapLBE9RPYGan62WSuuUI3u', 'download.jpg', 'city of birth', 'oceana', NULL),
-(14, 'max Halloway', 'maxblessed@ufc.org', '$2y$10$ZuT979iZzJdEdY.qWxaaRuwM8ryR6H9aybxjBiaCWZN30XN9JL9AC', '527d94df492c87037c2f424374de9056.jpg', 'favourite team', 'Barcelona FC', NULL),
-(15, 'winston churchil', 'winstonchurch@ww2.com', '$2y$10$lsqr.lSb5RBG7VHS6E.XQ.13CfxHpoQgcTOnb2rnfoJyzAU2VdMwi', 'Winston Churchill - Wikipedia.jpg', 'favourite team', 'Burnley', NULL),
-(16, 'christiano ronaldo', 'christiano@cr7.co.za', '$2y$10$nhUYLm.AXt1pxvQYDfa.uOfNItFHFbt8OyStPenC3ih8AxrkpjcbC', '', 'favourite_team', 'Real Madrid', NULL);
+INSERT INTO `users` (`id`, `full_name`, `email`, `password_hash`, `profile_picture`, `security_question`, `security_answer`, `cover_pic`, `bio`) VALUES
+(12, 'Pitso Tladi', 'pitsotladi71@gmail.com', '$2y$10$Lia0F6iXjt5RqiMrqdRWkOWe/I4Y9jZgPJAAKK/U85uI6U4tRe33m', 'OIP.webp', 'city of birth', 'Johannesburg', NULL, NULL),
+(13, 'george orwell', 'georgeOrwell@oceana.gov', '$2y$10$gwB5LU8riewZiXHYYl7DKumXgqqe8tOapLBE9RPYGan62WSuuUI3u', 'download.jpg', 'city of birth', 'oceana', NULL, NULL),
+(14, 'max Halloway', 'maxblessed@ufc.org', '$2y$10$aBXO2xjGdOt01UIRDdrgxeabPrOhRA82AASe93mEO.MnXXutC/bPm', '527d94df492c87037c2f424374de9056.jpg', 'favourite team', 'Barcelona FC', NULL, NULL),
+(15, 'winston churchil', 'winstonchurch@ww2.com', '$2y$10$lsqr.lSb5RBG7VHS6E.XQ.13CfxHpoQgcTOnb2rnfoJyzAU2VdMwi', 'Winston Churchill - Wikipedia.jpg', 'favourite team', 'Burnley', NULL, NULL),
+(16, 'christiano ronaldo', 'christiano@cr7.co.za', '$2y$10$nhUYLm.AXt1pxvQYDfa.uOfNItFHFbt8OyStPenC3ih8AxrkpjcbC', '', 'favourite_team', 'Real Madrid', NULL, NULL),
+(17, 'George st pierre', 'georgest@ufc.org', '$2y$10$dnQwI5OgaP/PpjaLvXj4pOuwUg.4bQAbKmyVg7cfSnjbMKAma4Y.y', 'GSP ☠️.jpg', 'City of birth', 'Canada', NULL, NULL),
+(20, 'samuel L jackson', 'JulesWinnfield@LA.com', '$2y$10$t.8SlluNF3pqnlFUjvFTx.AD2D5mdrA4ei6OgENVh4bPSTk05Ibj6', 'Samuel L_ Jackson.jpg', 'favourite team', 'chicago bulls', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -144,8 +155,9 @@ ALTER TABLE `friend_requests`
 --
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `sender_id` (`sender_id`),
-  ADD KEY `receiver_id` (`receiver_id`);
+  ADD KEY `fk_sender` (`sender_id`),
+  ADD KEY `fk_receiver` (`receiver_id`),
+  ADD KEY `fk_reply_to` (`reply_to_id`);
 
 --
 -- Indexes for table `posts`
@@ -181,19 +193,19 @@ ALTER TABLE `friend_requests`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Constraints for dumped tables
@@ -218,8 +230,9 @@ ALTER TABLE `friend_requests`
 -- Constraints for table `messages`
 --
 ALTER TABLE `messages`
-  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `fk_receiver` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `fk_reply_to` FOREIGN KEY (`reply_to_id`) REFERENCES `messages` (`id`),
+  ADD CONSTRAINT `fk_sender` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `posts`
